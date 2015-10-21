da86441fe6d64fc4b9315f746326b52a488dad3c
RodrigoJos <rodrigo_xd_100@hotmail.com>
2015-10-20 23:07:24 -0300

    Algumas funções de arquivo

--- EditorTexto/FileController.c
+++ EditorTexto/FileController.c
@@ -3,3 +3,45 @@ Descrição:
 Cabeçalho das funções com seus respectivos comentarios.
 */
 
+#include <stdio.h>
+
+FILE Criar (char nome[]) {
+FILE *arq;
+
+arq = fopen(nome,"wt");
+
+   if (arq == NULL)
+   {
+	printf("Erro ao criar o arquivo.\n");
+	return;
+   } else 
+     {
+    printf("Arquivo criado  com sucesso.\n");
+    return *arq;
+	 }
+}
+
+void Fechar (FILE *arq)
+{
+int status;
+status = fclose(arq);
+
+ if ( status == 0)
+	 printf("Arquivo fechado com sucesso.\n");
+ else
+	 printf("Erro no fechamento do arquivo.\n");
+}
+
+
+void Gravar (FILE *arq,char conteudo[])
+{
+int status;
+status=fputs(conteudo,arq);
+ if (status == EOF)
+	 printf("Erro na gravacao.\n");
+ else
+	 printf("Conteudo gravado com sucesso.\n");
+}
+ 
+
+
--- EditorTexto/FileController.h
+++ EditorTexto/FileController.h
@@ -3,3 +3,8 @@ Descrição:
 Cabeçalho das funções com seus respectivos comentarios.
 */
 
+FILE Criar (char nome[]); /*Cria um arquivo de texto,passando o nome do arquivo*/
+	
+void Fechar (FILE *arq);/*Fecha um arquivo,passando o mesmo*/
+
+void Gravar (FILE *arq,char conteudo[]);/*Grava um conteudo em um arquivo de texto,passando o arquivo e o conteudo*/
